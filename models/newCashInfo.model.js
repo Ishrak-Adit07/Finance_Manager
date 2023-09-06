d4f4a4d3fe9bf03b47bb343d4eb7723cbbe776e8
Ishrak Adit <ishrak.adit07@gmail.com>
2023-09-07 00:31:51 +0600

    New Cash Info model commit

---
+++ models/newCashInfo.model.js
@@ -0,0 +1,17 @@
+exports.newIncomeInfo = {
+    amount: 0,
+    wallet: 1,
+    type: "Salary"
+};
+
+exports.newExpenseInfo = {
+    amount: 0,
+    wallet: 1,
+    type: "Food & Drinks"
+};
+
+exports.newBudgetInfo + {
+    amount: 0, 
+    purpose: "Project",
+    lastUpdatedOn: '23-08-2023'
+};

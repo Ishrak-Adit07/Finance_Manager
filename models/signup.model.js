45d3f3ba12116661e093e92fd58000ec6c838865
Ishrak Adit <ishrak.adit07@gmail.com>
2023-09-07 00:32:23 +0600

    Sign up model commit

---
+++ models/signup.model.js
@@ -0,0 +1,12 @@
+var newUserInfo = {
+    name: "Dummy name",
+    mail: "dummymail@gmail.com",
+    dob: '1932-04-20 02:02:46',
+    gender: 'M',
+    address: "Address",
+    job: "Job",
+    username: "Username",
+    password: "Password"
+}
+
+module.exports = {newUserInfo};

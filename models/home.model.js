c4812680cba4c968788b617b0e2e5985e50b44b0
Ishrak Adit <ishrak.adit07@gmail.com>
2023-09-07 00:29:48 +0600

    Home models commit

---
+++ models/home.model.js
@@ -0,0 +1,12 @@
+var recentActivities = [
+    {
+        typeAndId: "inc/exp_userid_date_time",
+        wallet: 1, 
+        type: "Salary", 
+        amount: 0,
+        date: "2023-08-02 02:50:35",
+        incOrExp: "Income"
+    }
+];
+
+module.exports = {recentActivities};

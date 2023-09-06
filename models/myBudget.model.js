a3c51e190edf62b833e2f804ab20c2a837591e30
Ishrak Adit <ishrak.adit07@gmail.com>
2023-09-07 00:31:14 +0600

    My budgets model commit

---
+++ models/myBudget.model.js
@@ -0,0 +1,13 @@
+var currentBudgets = [
+    {
+        budgetID: 1, 
+        budgetName: "Salary", 
+        amount: 100,
+        spent: 0,
+        left: 0,
+        createdOn: "2023-08-02 02:50:35",
+        lastUpdatedOn: "2023-08-02 02:50:35"
+    }
+];
+
+module.exports = {currentBudgets};

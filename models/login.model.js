351cdd54fc3070b030a3e4a398283d0e9818031c
Ishrak Adit <ishrak.adit07@gmail.com>
2023-09-07 00:30:22 +0600

    Login model commit

---
+++ models/login.model.js
@@ -0,0 +1,14 @@
+var currentUser ={
+    name: "Ishrak Adit",
+    userID: 1234,
+    age: 22,
+    wallets: 0,
+    amounts: [0],
+    budgets: 0
+};
+
+var loggedPassword = "12345";
+
+module.exports = {currentUser, loggedPassword};
+
+//Test commit comment

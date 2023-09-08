0dd658c8112966d7aace2c8f3bbace4d2760549e
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2023-09-08 11:21:13 +0600

    Delete wallet execute commit

--- models/deleteInfo.model.js
+++ models/deleteInfo.model.js
@@ -1,4 +1,5 @@
 exports.deleteWalletInfo = {
     walletID: 1, 
+    shiftWalletID: 2,
     amount: 100
 };
--- routes/deleteWallet.route.js
+++ routes/deleteWallet.route.js
@@ -14,12 +14,45 @@ router.get("/", (req, res)=>{
     res.render(path.join(__dirname+"/../views/deleteWallet.ejs"), {currentUser});
 });
 
-router.post("/", (req, res)=>{
+router.post("/", async(req, res)=>{
     deleteWalletInfo.walletID = Number(req.body.walletID);
     console.log("Wallet to be deleted: "+deleteWalletInfo.walletID);
+    deleteWalletInfo.shiftWalletID = Number(req.body.shiftWalletID);
+    if(deleteWalletInfo.shiftWalletID==null) deleteWalletInfo.shiftWalletID = 0;
+    console.log("Shift Wallet: "+deleteWalletInfo.shiftWalletID);
 
-    deleteWalletInfo.amount = currentUser.amounts[deleteWalletInfo.walletID];
-    console.log(deleteWalletInfo.amount);
+    if(deleteWalletInfo.walletID <= currentUser.wallets && deleteWalletInfo.shiftWalletID <= currentUser.wallets){
+        try{
+            const deleteWalletFromTablesQuery = `BEGIN
+                                                    DELETE_WALLET_FROM_TABLES(:USERID, :WALLETID);
+                                                 END;`;
+            const deleteWalletFromTablesBinds = {
+                USERID: currentUser.userID,
+                WALLETID: deleteWalletInfo.walletID
+            };
+    
+            const result1 = await runProcedure(deleteWalletFromTablesQuery, deleteWalletFromTablesBinds);
+
+            const updateTablesAfterDeleteWalletQuery = `BEGIN
+                                                            UPDATE_TABLES_AFTER_DELETE_WALLET(:USERID, :WALLETID);
+                                                        END;`;
+            const updateTablesAfterDeleteWalletBinds = {
+                USERID: currentUser.userID,
+                WALLETID: deleteWalletInfo.walletID
+            };
+    
+            const result2 = await runProcedure(updateTablesAfterDeleteWalletQuery, updateTablesAfterDeleteWalletBinds);
+            
+            for(var i=deleteWalletInfo.walletID; i<currentUser.wallets; i++){
+                currentUser.amounts[i] = currentUser.amounts[i+1];
+            }
+            currentUser.amounts.pop();
+            currentUser.wallets -= 1;
+        }catch(error){
+            console.log(error);
+            console.error("deleteWalletFromTablesQuery/updateTablesAfterDeleteWalletQuery Not Executed");
+        }
+    }
 
     res.render(path.join(__dirname+"/../views/currentStatus.ejs"), {currentUser});
 });

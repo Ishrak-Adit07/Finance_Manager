60d308477aea77e6cd231ebdfb8068969f0d8e3d
Ishrak Adit <ishrak.adit07@gmail.com>
2023-09-11 00:22:25 +0600

    Edit Budget Solve Commit

--- routes/editBudgets.route.js
+++ routes/editBudgets.route.js
@@ -20,7 +20,27 @@ const { runQuery } = require("../dbConnection/runFunctions");
 const { currentBudgets } = require("../models/myBudget.model");
 const { editBudgetInfo } = require("../models/editBudgets.model");
 
-router.get("/", (req, res) => {
+router.get("/", async(req, res) => {
+  const getMyBudgetInfosQuery = `SELECT * FROM "FINANCEMANAGER"."Budgets"
+                                   WHERE "UserID" LIKE '${currentUser.userID}'`;
+  let getMyBudgetInfosQueryResult = await runQuery(getMyBudgetInfosQuery);
+
+  currentBudgets.length = 0;
+  for (var i in getMyBudgetInfosQueryResult) {
+    let param1 = getMyBudgetInfosQueryResult[i][1];
+    let param2 = getMyBudgetInfosQueryResult[i][2];
+    let param3 = getMyBudgetInfosQueryResult[i][3];
+    let param4 = getMyBudgetInfosQueryResult[i][4];
+    let param5 = getMyBudgetInfosQueryResult[i][5];
+    let param6 = String(getMyBudgetInfosQueryResult[i][6]);
+    let param7 = String(getMyBudgetInfosQueryResult[i][7]);
+    param6 = param6.substring(0, 25);
+    param7 = param7.substring(0, 25);
+
+    let currBudget = { param1, param2, param3, param4, param5, param6, param7 };
+    currentBudgets.push(currBudget);
+  }
+
   res.render(path.join(__dirname + "/../views/editBudgets.ejs"), {
     currentUser,
     currentBudgets,
@@ -84,6 +104,7 @@ router.post("/", async (req, res) => {
     console.log(saveFromSpentQuery);
     let saveFromSpentQueryResult = await runQuery(saveFromSpentQuery);
 
+    /*
     if (editBudgetInfo.totalAmountUpdate != 0) {
       currentBudgets[editBudgetInfo.budgetID].amount =
         editBudgetInfo.totalAmountUpdate;
@@ -103,6 +124,7 @@ router.post("/", async (req, res) => {
       currentBudgets[editBudgetInfo.budgetID].lastUpdatedOn =
         budgetEditDateInput;
     }
+    */
   }
 
   res.redirect("/home");
